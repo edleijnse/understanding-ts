e828be2663e39768dfdeaa2108253a9e234834bf
edleijnse <Rax03Dam>
2021-10-24 20:33:16 +0200

    Aliases

--- app.ts
+++ app.ts
@@ -1,21 +1,14 @@
-type Combinable = number | string;
-type ConversionDescriptor = 'as-number' | 'as-text';
-function combine(input1: Combinable, input2: Combinable, resultConversion: ConversionDescriptor) {
-    let result;
-    if ((typeof input1 === 'number' && typeof input2 === 'number') || (resultConversion === 'as-number')){
-        result = +input1 + +input2;
-    } else {
-        result = input1.toString() + input2.toString();
-    }
-
-    return result;
+function add(n1: number, n2: number){
+    return n1 + n2;
+}
+function printResult(num: number){
+    console.log('Result: ' + num)
 }
 
-const combinedAges = combine(30, 26, 'as-number');
-console.log(combinedAges);
+printResult(add(5,12));
 
-const combinedStringAges = combine('30', '26', 'as-number');
-console.log(combinedStringAges);
+let combineValues: (a: number, b: number) => number;
 
-const combinedNames = combine('Max', 'Anna', 'as-text');
-console.log(combinedNames);
+combineValues = add;
+
+console.log(combineValues(8,8));
